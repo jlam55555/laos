fbf27733f84f94243c27b2b4dc3fc1db9e80fc25
Jonathan Lam <jlam55555@gmail.com>
2023-05-15 21:53:52 -0700

    Define virtual memory constants

--- src/kernel/diag/mm.c
+++ src/kernel/diag/mm.c
@@ -3,6 +3,16 @@
 #include "arch/x86_64/registers.h"
 #include "common/libc.h"
 
+/**
+ * TODO: move this into a x86_64 memory management header
+ */
+#define VM_ADDR_SPACE_SZ 48
+#define VM_MAX_BIT (1lu << (VM_ADDR_SPACE_SZ - 1))
+#define VM_CANON_BITS (~(VM_MAX_BIT - 1))
+#define VM_PG_LV 4
+#define VM_PG_SZ 4096
+#define VM_PG_SZ_BITS 12
+
 static int var_data = 1;
 static int var_bss;
 
@@ -65,6 +75,11 @@ static inline void _get_pt_addr(struct cr3_register_pcide *reg_cr3) {
  * Used for the page directory table (level 2), the page
  * directory pointer table (level 3), and the PML4 (level 4)
  * tables. Not used for the page table (level 1).
+ *
+ * The avl* fields are ignored by the architecture, and the
+ * reserved fields must not be used (set to zero).
+ *
+ * See IA32/64 Reference, Volume 3A, 4-32
  */
 struct pmlx_entry {
   uint8_t p : 1;
@@ -74,6 +89,8 @@ struct pmlx_entry {
   uint8_t pcd : 1;
   uint8_t a : 1;
   uint8_t avl : 1;
+  // Reserved on PML4, used to indicate 1GiB/2MiB pages
+  // on PML3 and PML2, respectively.
   uint8_t ps : 1;
   uint8_t avl2 : 4;
   // Assuming a 52-bit physical address space.
@@ -88,27 +105,28 @@ struct pmlx_entry {
  * through to bit 63.
  */
 void *va_canonicalize(void *addr) {
-  if ((uint64_t)addr & 0x0000800000000000) {
-    return (void *)((uint64_t)addr | 0xffff000000000000);
+  if ((uint64_t)addr & VM_MAX_BIT) {
+    return (void *)((uint64_t)addr | VM_CANON_BITS);
   }
   return addr;
 }
 
 void _print_pt(struct pmlx_entry *pmlx, int level, void *prev_va) {
-  for (uint64_t i = 0; i < (4096 / sizeof *pmlx); ++i) {
+  for (uint64_t i = 0; i < (VM_PG_SZ / sizeof *pmlx); ++i) {
     if (!pmlx[i].p) {
       continue;
     }
 
-    void *va = va_canonicalize(
-        (void *)((uint64_t)prev_va | ((uint64_t)i << (9 * level + 12))));
-    void *pa = (void *)((uint64_t)pmlx[i].addr << 12);
+    void *va =
+        va_canonicalize((void *)((uint64_t)prev_va |
+                                 ((uint64_t)i << (9 * level + VM_PG_SZ_BITS))));
+    void *pa = (void *)((uint64_t)pmlx[i].addr << VM_PG_SZ_BITS);
 
-    // Refers to a lower-level page table.
-    if (level == 3 || (level && !pmlx[i].ps)) {
+    // Refers to a lower-level page table. Recurse.
+    if (level && !pmlx[i].ps) {
       _print_pt(pa, level - 1, va);
     }
-    // Refers to a page.
+    // Refers to a single page.
     else {
       printf("PML%d: %lx -> %lx (%lx)\r\n", level, va, pa, pmlx);
     }
@@ -118,6 +136,7 @@ void _print_pt(struct pmlx_entry *pmlx, int level, void *prev_va) {
 void print_mm(void) {
   int var_stk;
 
+  // Print various details about the memory mapping.
   printf("\r\nMemory mapping\r\n");
   printf("Text:   0x%lx\r\n", print_mm);
   printf("Kernel: %ld bytes\r\n", -(long int)print_mm);
@@ -126,12 +145,12 @@ void print_mm(void) {
   printf("Stack:  0x%lx\r\n", &var_stk);
   printf("Stack grows downwards: %d\r\n", _stk_grows_downwards());
 
+  // Print page table.
   struct cr3_register_pcide reg_cr3;
   _get_pt_addr(&reg_cr3);
-
-  printf("PT:     0x%lx\r\n",
-         (struct pmlx_entry *)((uint64_t)reg_cr3.base << 12));
-  _print_pt((struct pmlx_entry *)((uint64_t)reg_cr3.base << 12), 3, NULL);
+  printf("PT:     0x%lx\r\n", (uint64_t)reg_cr3.base << VM_PG_SZ_BITS);
+  _print_pt((struct pmlx_entry *)((uint64_t)reg_cr3.base << VM_PG_SZ_BITS),
+            VM_PG_LV - 1, NULL);
 
   printf("\r\n");
 }
