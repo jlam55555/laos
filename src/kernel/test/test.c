a8966ed2b18dca65041e3e1b0200bf4380cdb952
Jonathan Lam <jlam55555@gmail.com>
2023-09-03 12:39:47 -0700

    Add tests for test pattern matching

--- src/kernel/test/test.c
+++ src/kernel/test/test.c
@@ -101,10 +101,93 @@ void run_tests(const char *selection) {
 #endif // RUNTEST
 }
 
-// Meta-test. Test different patterns that would match itself.
 DEFINE_TEST(test__pattern_matcher) {
   const char *pat1 = "test__";
+  const char *pat2 = "";
+  const char *pat3 = "^test__";
+  const char *pat4 = "pattern_matcher";
+  const char *pat5 = "pattern_matcher$";
+  const char *pat6 = "test__pattern_matcher$";
+  const char *pat7 = "^test__pattern_matcher$";
+  const char *pat8 = "^test__pattern_matcher";
+  const char *pat9 = "^1test__pattern_matcher";
+  const char *pat10 = "^test__pattern_matcher1";
+  const char *pat11 = "test__pattern_matcher1$";
+  const char *pat12 = "test__pattern_matcher1";
+  const char *pat13 = "__";
+  const char *pat14 = "match";
+
   const struct test_info test1 = {.name = "test__pattern_matcher"};
 
   TEST_ASSERT(_test_is_match(&test1, pat1));
+  TEST_ASSERT(_test_is_match(&test1, pat2));
+  TEST_ASSERT(_test_is_match(&test1, pat3));
+  TEST_ASSERT(_test_is_match(&test1, pat4));
+  TEST_ASSERT(_test_is_match(&test1, pat5));
+  TEST_ASSERT(_test_is_match(&test1, pat6));
+  TEST_ASSERT(_test_is_match(&test1, pat7));
+  TEST_ASSERT(_test_is_match(&test1, pat8));
+  TEST_ASSERT(!_test_is_match(&test1, pat9));
+  TEST_ASSERT(!_test_is_match(&test1, pat10));
+  TEST_ASSERT(!_test_is_match(&test1, pat11));
+  TEST_ASSERT(!_test_is_match(&test1, pat12));
+  TEST_ASSERT(_test_is_match(&test1, pat13));
+  TEST_ASSERT(_test_is_match(&test1, pat14));
+
+  const struct test_info test2 = {.name = "test__foo"};
+
+  TEST_ASSERT(_test_is_match(&test2, pat1));
+  TEST_ASSERT(_test_is_match(&test2, pat2));
+  TEST_ASSERT(_test_is_match(&test2, pat3));
+  TEST_ASSERT(!_test_is_match(&test2, pat4));
+  TEST_ASSERT(!_test_is_match(&test2, pat5));
+  TEST_ASSERT(!_test_is_match(&test2, pat6));
+  TEST_ASSERT(!_test_is_match(&test2, pat7));
+  TEST_ASSERT(!_test_is_match(&test2, pat8));
+  TEST_ASSERT(!_test_is_match(&test2, pat9));
+  TEST_ASSERT(!_test_is_match(&test2, pat10));
+  TEST_ASSERT(!_test_is_match(&test2, pat11));
+  TEST_ASSERT(!_test_is_match(&test2, pat12));
+  TEST_ASSERT(_test_is_match(&test2, pat13));
+  TEST_ASSERT(!_test_is_match(&test2, pat14));
+
+  const struct test_info test3 = {.name = "bar__hello"};
+
+  TEST_ASSERT(!_test_is_match(&test3, pat1));
+  TEST_ASSERT(_test_is_match(&test3, pat2));
+  TEST_ASSERT(!_test_is_match(&test3, pat3));
+}
+
+DEFINE_TEST(test__conjunctive_simple_patterns) {
+  const char *pat1 = "foo";
+  const char *pat2 = "bar";
+  const char *pat3 = "foo,bar";
+
+  const struct test_info test1 = {.name = "foo"};
+  const struct test_info test2 = {.name = "bar"};
+
+  TEST_ASSERT(_test_is_match(&test1, pat1));
+  TEST_ASSERT(!_test_is_match(&test1, pat2));
+  TEST_ASSERT(_test_is_match(&test1, pat3));
+
+  TEST_ASSERT(!_test_is_match(&test2, pat1));
+  TEST_ASSERT(_test_is_match(&test2, pat2));
+  TEST_ASSERT(_test_is_match(&test2, pat3));
+}
+
+DEFINE_TEST(test__conjunctive_complex_patterns) {
+  const char *pat1 = "^foo$";
+  const char *pat2 = "ba";
+  const char *pat3 = "^foo$,ba";
+
+  const struct test_info test1 = {.name = "foo"};
+  const struct test_info test2 = {.name = "bar"};
+
+  TEST_ASSERT(_test_is_match(&test1, pat1));
+  TEST_ASSERT(!_test_is_match(&test1, pat2));
+  TEST_ASSERT(_test_is_match(&test1, pat3));
+
+  TEST_ASSERT(!_test_is_match(&test2, pat1));
+  TEST_ASSERT(_test_is_match(&test2, pat2));
+  TEST_ASSERT(_test_is_match(&test2, pat3));
 }
