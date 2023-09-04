3417bc1ce6f179be6050d5ec7e848a4326d467a5
Jonathan Lam <jlam55555@gmail.com>
2023-09-03 19:07:43 -0700

    Add tests for util functions

--- src/kernel/common/util.h
+++ src/kernel/common/util.h
@@ -3,10 +3,11 @@
 
 #include <stdint.h>
 
-// Read the hardware timestamp counter. We want this to be
-// inline to decrease overhead. It may be better to have
-// this as a pure assembly function, but inline assembly
-// shouldn't be too far off.
+/**
+ * Read the hardware timestamp counter. We want this to be inline to decrease
+ * overhead. It may be better to have this as a pure assembly function, but
+ * inline assembly shouldn't be too far off.
+ */
 inline uint64_t read_tsc(void) {
   uint64_t res;
   __asm__ volatile("rdtsc\n\t"          // Returns the time in EDX:EAX.
@@ -39,12 +40,12 @@ inline uint8_t inb(uint16_t port) {
  * Note that the end result is not parenthesized into a single expression.
  */
 #define JOIN2(delim, a, b) a delim b
-#define JOIN3(delim, a, ...) a delim JOIN2(__VA_ARGS__)
-#define JOIN4(delim, a, ...) a delim JOIN3(__VA_ARGS__)
-#define JOIN5(delim, a, ...) a delim JOIN4(__VA_ARGS__)
-#define JOIN6(delim, a, ...) a delim JOIN5(__VA_ARGS__)
-#define JOIN7(delim, a, ...) a delim JOIN6(__VA_ARGS__)
-#define JOIN8(delim, a, ...) a delim JOIN7(__VA_ARGS__)
+#define JOIN3(delim, a, ...) a delim JOIN2(delim, __VA_ARGS__)
+#define JOIN4(delim, a, ...) a delim JOIN3(delim, __VA_ARGS__)
+#define JOIN5(delim, a, ...) a delim JOIN4(delim, __VA_ARGS__)
+#define JOIN6(delim, a, ...) a delim JOIN5(delim, __VA_ARGS__)
+#define JOIN7(delim, a, ...) a delim JOIN6(delim, __VA_ARGS__)
+#define JOIN8(delim, a, ...) a delim JOIN7(delim, __VA_ARGS__)
 
 /** Stringify a macro. E.g., one that comes from a -Dmacro=xyz gcc arg.
  * https://gcc.gnu.org/onlinedocs/cpp/Stringizing.html
