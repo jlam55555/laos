4700af42c854f31b4ee981f6e850a8067400b48a
Jonathan Lam <jlam55555@gmail.com>
2023-10-01 14:46:21 -0400

    Improved documentation for scheduler

--- src/kernel/sched/sched.h
+++ src/kernel/sched/sched.h
@@ -1,13 +1,74 @@
 /**
- * Very simple round-robin task scheduler.
+ * Very simple round-robin task scheduler for kernel threads. This does not have
+ * any userspace thread/process semantics.
  *
- * TODO(jlam55555): Set up unit tests.
- * TODO(jlam55555): Set up quanta.
- * TODO(jlam55555): Document idle task.
- * TODO(jlam55555): Document scheduler "valid" state. Only after calling
- * `sched_task_switch_nostack()` for the first time will there be a current task
- * in a scheduler. Similarly, after calling `sched_destroy()` there will be no
- * more tasks on this scheduler.
+ * Each task ("thread") has its own stack, instruction pointer (implicitly
+ * stored on the stack when scheduling away), and no memory protections. A
+ * scheduler will have exactly one task scheduled (running) at a time (after the
+ * bootstrap process and before destruction).
+ *
+ * To perform scheduling, you must first initialize a scheduler object. Tasks
+ * can be added to this scheduler (either via `sched_clone_task()` or
+ * `sched_create_task()`, which are like Linux's `clone()` and `exec*()`
+ * syscalls, respectively). Tasks can be scheduled using `sched_choose_task()`
+ * and `sched_task_switch()`.
+ *
+ * In general, you will not need to worry about creating a scheduler object, the
+ * idle task, or bootstrapping process -- the interfaces to initialize a
+ * scheduler are mostly for unit testing. There is a global main task scheduler
+ * which all scheduling operations are implied to act on.
+ *
+ * This kernel uses pre-emptive scheduling. I.e., the timer interrupt will force
+ * a process to call `schedule()` if it has exceeded its quantum.
+ *
+ * TODO(jlam55555): Set up quanta/pre-emptive scheduling.
+ * TODO(jlam55555): Set up global main scheduler.
+ * TODO(jlam55555): Implement `clone()` API.
+ *
+ * =============================================================================
+ * Bootstrapping process
+ * =============================================================================
+ * There is some special handling around entering the scheduler for the first
+ * time -- this is called the "bootstrapping process". There are two ways to do
+ * this. Calling `sched_task_switch(_nostack)()` for the first time will
+ * automatically bootstrap the given thread if there were no previously running
+ * tasks. However, doing so will also discard the current stack, which may be
+ * undesirable -- thus, the helper function `sched_bootstrap_task()` is provided
+ * to form an `exec*()`-like interface to enter the scheduler for the first
+ * time. This is used in the global main task scheduler.
+ *
+ * Bootstrapping a scheduler when another one is already running, or destroying
+ * a bootstrapped scheduler are both UB -- we expect that there is exactly one
+ * kernel-level task scheduler that persists the entire lifetime of the
+ * operating system once it has bootstrapped. (This is not the case for unit
+ * tests, which use a pseudo-bootstrapping process. This may also change in the
+ * future if scheduling needs become more complicated.)
+ *
+ * =============================================================================
+ * Idle task
+ * =============================================================================
+ * In general, the round-robin scheduler chooses the first task on the runnable
+ * queue to schedule. If not, then it'll choose the previously-running task, if
+ * it is not in a blocked state (i.e., it was pre-empted). But what if there are
+ * no runnable tasks and the previously-running task is blocked?
+ *
+ * This motivates an idle task, which is a regular task with some special
+ * properties: it always exists and is always runnable/running (never blocked).
+ * It will thus be the target for scheduling if no other task is schedulable.
+ * Like a sentinel node, this more cleanly handles this edge case than trying to
+ * write the logic to manually wait for a task to be schedulable.
+ *
+ * The idle task is not provided by the scheduler -- the implementor must create
+ * the idle task. Here is an example of how to bootstrap into the scheduler with
+ * an idle task:
+ *
+ * ```
+ * static void _idle_task(void) { for (;;) { __asm__("hlt"); } }
+ * ...
+ * struct scheduler scheduler;
+ * sched_add_task(&scheduler, _idle_task);
+ * sched_bootstrap_task(&scheduler);
+ * ```
  */
 #ifndef SCHED_SCHED_H
 #define SCHED_SCHED_H
@@ -15,7 +76,7 @@
 #include "common/list.h"
 
 /**
- * Round-robin scheduler.
+ * Round-robin task scheduler.
  */
 struct sched_task;
 struct scheduler {
@@ -26,8 +87,9 @@ struct scheduler {
 };
 
 /**
- * TODO(jlam55555): Document this.
- * TODO(jlam55555): Allow tasks to have a desc/name.
+ * A kernel thread.
+ *
+ * TODO(jlam55555): Allow threads to have a name.
  */
 struct sched_task {
   struct list_head ll;
